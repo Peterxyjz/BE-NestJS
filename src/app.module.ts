4d666d2d3cb55f84fd4c93dccf0b1982658e9dc2
Peterxyjz <huyq09@gmail.com>
2024-10-16 11:09:40 +0700

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
